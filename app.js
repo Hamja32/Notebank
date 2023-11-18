fe7f1f1793fe22bc181ca4e00c81ccaae5cd7378
Hamza khan <150890458+Hamja32@users.noreply.github.com>
2023-11-18 17:50:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -34,10 +34,10 @@ showNotes();
          
 
         let html =  "";
-        notesObj.forEach(function (element,index,titleNew){
+        notesObj.forEach(function (element,index){
             html += ` <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
             <div class="card-body">
-                <h5 class="card-title">${index + 1} + get.date();</h5>
+                <h5 class="card-title">${index + 1}</h5>
                 <p class="card-text">${element}</p>
                 <button id="${index}" onclick="deleteNotes(this.id)" class="delete btn">Delete</button>
             </div>
